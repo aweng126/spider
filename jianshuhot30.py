d72f3cc6d082e13da28d513f3b303649607fc0fb
aweng126 <13075313106@163.com>
2017-06-27 08:44:58 +0800

    文件保存

--- jianshuhot30.py
+++ jianshuhot30.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from requests.exceptions import RequestException
 import re
@@ -16,12 +18,11 @@ def parse_one_page(html):
         '<li.*?blue-link".*?>(.*?)</a>.*?title".*?href="(.*?)">(.*?)</a>.*?abstract">(.*?)</p>.*?ic-list-read">.*?</i>(.*?)</a>.*?ic-list-comments.*?</i>(.*?)</a>.*?ic-list-like.*?</i>(.*?)</span>.*?ic-list-money.*?</i>(.*?)</span>.*?</li>',
         re.S)
     items=re.findall(pattern,html)
-    #print(items)
 
     for item in items:
         yield {
             'author':item[0],
-            'link':item[1],
+            'link':"http://www.jianshu.com"+item[1],
             'title':item[2],
             'abstract':item[3].strip(),
             'read-num':item[4].strip(),
@@ -30,11 +31,18 @@ def parse_one_page(html):
             'money-num':item[7]
         }
 
+def write_to_file(content):
+    with open('result.txt','a',encoding='utf-8') as f:
+        f.write(json.dumps(content,ensure_ascii=False)+'\n')
+        f.close()
+
+
 def main():
     url = 'http://www.jianshu.com/trending/monthly?utm_medium=index-banner-s&utm_source=desktop'
     html = get_one_page(url)
     for item in parse_one_page(html):
         print(item)
+        write_to_file(item)
 
 
 if __name__=='__main__':
