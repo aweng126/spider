16211b958d2a00870c3144aa01b5a86e1cc5fc59
aweng126 <13075313106@163.com>
2017-07-23 18:33:55 +0800

    爬取知乎某答案下所有答案更新   多问题字典存储，设置存储位置

--- zhihuSpider.py
+++ zhihuSpider.py
@@ -86,7 +86,10 @@ def get_all_pic_url(question_num, answer_offset,answer_limit):
           '2Cmark_infos%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B*%5D.author.follower_count%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&' \
           'offset={offset}&limit={limit}&sort_by=default'
     response = session.get(url.format(qnum=question_num,offset=answer_offset,limit=answer_limit), headers=headers, allow_redirects=False)
+
+    print('json_response', response)
     json_response = response.json()
+
     answer = json_response['data']
     pattern = re.compile(r'data-original=\"https\:(.*?)\.(jpg|png)"')
     urls = [];
@@ -125,16 +128,22 @@ def download_url_picture(img_url, file_name, folder_name, folder_base):
 @answer_num:想下载的回答数
 @folder_name:文件夹名字
 @folder_base:文件夹所在位置
+@pic_num:照片编号
+return
+@picnum:本次照片结束后编号，方便下次继续请求给文件命名
 '''
-def start_kanmeizi(question_num,answer_offset, answer_limit, folder_name, folder_base):
+pic_num=0
+def start_kanmeizi(question_num,answer_offset, answer_limit, folder_name, folder_base,pic_num):
     urls = get_all_pic_url(question_num, answer_offset,answer_limit)
     for i in range(0, len(urls)):
-        download_url_picture(urls[i], folder_name + '('+str(i)+')', folder_name, folder_base)
-        print(folder_name,'第', i, '张图片已经下载完成')
-
+        download_url_picture(urls[i], folder_name + '('+str(pic_num)+')', folder_name, folder_base)
+        pic_num=pic_num+1
+        print(folder_name,'第', pic_num, '张图片已经下载完成')
+    return  pic_num
 
 if __name__ == '__main__':
-    file_base = 'C:\\Users\kingwen\Desktop'
+    # file_base = 'C:\\Users\kingwen\Desktop'
+    file_base = 'H:\\pic'
     if isLogin():
         print('您已经登录')
     else:
@@ -144,4 +153,43 @@ if __name__ == '__main__':
         print('_xsrf =', _xsrf)
         captcha = get_captcha()
         login(email, password, _xsrf, captcha)
-    start_kanmeizi(39655195,-1, 20, '如何拍出不错的旅行风景照片？', file_base)
+
+    # https: // www.zhihu.com / question / 23305736   毕业照要怎么拍才好看？
+    # https: // www.zhihu.com / question / 49364343   给好看的女生拍照是种怎样的体验？
+    #https://www.zhihu.com/question/35931586  你的日常搭配是什么样子？
+    # https: // www.zhihu.com / question / 38285230  有一群漂亮的朋友是什么体验？
+    # https://www.zhihu.com/question/40273344  女生有一头短发是种怎样的体验？
+    # https://www.zhihu.com/question/24463692  如何自拍才能拍得好看？
+    #https://www.zhihu.com/question/31785374  和会拍照的人旅游是种怎样的体验？
+    # https://www.zhihu.com/question/22918070 女生怎么健身锻造好身材？
+    #https://www.zhihu.com/question/62536107  有一个长得帅又会拍照的男朋友是一番怎样的体验？
+    #https://www.zhihu.com/question/28467579  做一个可爱的女孩子是一种什么体验？
+    #https://www.zhihu.com/question/34243513  你见过最漂亮的女生长什么样？
+    # https: // www.zhihu.com / question / 28560777  有一个很漂亮的女朋友是什么体验？
+    # https://www.zhihu.com/question/28202126  有很漂亮的亲姐妹是种什么样的体验？
+    # https://www.zhihu.com/question/28441060  你见过的最美的侧脸是什么样子？
+    # https://www.zhihu.com/question/21100397 如何才能拍出一张特别好看的自拍照？
+
+    question_dict={
+        # '35931586': '你的日常搭配是什么样子？',
+        # '38285230':  '有一群漂亮的朋友是什么体验？',
+        # '40273344 ' :'女生有一头短发是种怎样的体验？',
+        # '24463692' : '如何自拍才能拍得好看？',
+        '31785374' : '和会拍照的人旅游是种怎样的体验？',
+        '22918070' :'女生怎么健身锻造好身材？',
+        '62536107' : '有一个长得帅又会拍照的男朋友是一番怎样的体验？',
+        '28467579' : '做一个可爱的女孩子是一种什么体验？',
+        '34243513'  :'你见过最漂亮的女生长什么样？',
+        '28560777'  :'有一个很漂亮的女朋友是什么体验？',
+        '28202126'  :'有很漂亮的亲姐妹是种什么样的体验？',
+        '28441060'  :'你见过的最美的侧脸是什么样子？',
+        '21100397' :'如何才能拍出一张特别好看的自拍照？'
+    }
+
+    for key in question_dict.keys():
+        pic_num=0
+        print(key,question_dict[key])
+        for i in range(1,20):
+            pic_num=start_kanmeizi(key , 20*(i-1), 20*i,question_dict[key], file_base,pic_num)
+
+        print('下一个答案')
